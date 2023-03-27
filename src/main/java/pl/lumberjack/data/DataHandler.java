ce85945edc00b24812c2f3b62297cddfb43e07e0
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-03-27 19:36:00 +0200

    Plugin
    - Added basic lumberjack plugin

---
+++ src/main/java/pl/lumberjack/data/DataHandler.java
@@ -0,0 +1,47 @@
+package pl.lumberjack.data;
+
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import pl.lumberjack.LumberJack;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class DataHandler {
+
+    private int treeMaxHeight = 60;
+    private int woodDestroyInterval = 2;
+    private int leavesDestroyInterval = 1;
+    private int woodRange = 2;
+    private int leavesRange = 5;
+    private int requiredLeaves = 10;
+    private List<Material> axes = new ArrayList<>();
+    private String permission = "lumberjack.use";
+
+    public void loadConfig() {
+        File configFile = new File(LumberJack.getInstance().getDataFolder(), "config.yml");
+        if(!configFile.exists()) {
+            LumberJack.getInstance().saveResource("config.yml", false);
+        }
+        YamlConfiguration yml = YamlConfiguration.loadConfiguration(configFile);
+        this.treeMaxHeight = yml.getInt("config.maxTreeHeight");
+        this.woodDestroyInterval = yml.getInt("config.woodDestroyInterval");
+        this.leavesDestroyInterval = yml.getInt("config.leavesDestroyInterval");
+        this.woodRange = yml.getInt("config.woodRange");
+        this.leavesRange = yml.getInt("config.leavesRange");
+        this.requiredLeaves = yml.getInt("config.requiredLeaves");
+        this.permission = yml.getString("config.permission");
+        List<String> pot_axes = yml.getStringList("config.axes");
+        for(String axe : pot_axes) {
+            if(Material.getMaterial(axe) == null) {
+                LumberJack.getInstance().getLogger().severe("Not found axe material: " + axe);
+            } else {
+                axes.add(Material.getMaterial(axe));
+            }
+        }
+    }
+
+}
---
+++ src/main/java/pl/lumberjack/permissions/PermissionManager.java
@@ -0,0 +1,26 @@
+package pl.lumberjack.permissions;
+
+import lombok.Getter;
+import org.bukkit.permissions.Permission;
+import pl.lumberjack.LumberJack;
+
+import java.util.HashMap;
+
+@Getter
+public class PermissionManager {
+
+    private final HashMap<String, Permission> permissions = new HashMap<>();
+
+    public PermissionManager() {
+        registerPermission(LumberJack.getInstance().getDataHandler().getPermission(), "Use LumberJack feature");
+    }
+
+    public void registerPermission(String permission, String description) {
+        permissions.put(permission, new Permission(permission, description));
+    }
+
+    public Permission getPermission(String permission) {
+        return permissions.getOrDefault(permission, null);
+    }
+
+}
