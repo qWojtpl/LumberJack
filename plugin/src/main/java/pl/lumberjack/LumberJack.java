e91aa1c7ae9b91c3cf38f3a5f0083cb2e4e57b27
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-03-27 19:37:35 +0200

    Plugin
    - Added basic lumberjack plugin
    - Moved source to /plugin/

---
+++ plugin/src/main/java/pl/lumberjack/LumberJack.java
@@ -0,0 +1,34 @@
+package pl.lumberjack;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+import pl.lumberjack.data.DataHandler;
+import pl.lumberjack.events.Events;
+import pl.lumberjack.permissions.PermissionManager;
+
+@Getter
+public final class LumberJack extends JavaPlugin {
+
+    private static LumberJack main;
+    private PermissionManager permissionManager;
+    private DataHandler dataHandler;
+
+    @Override
+    public void onEnable() {
+        main = this;
+        this.dataHandler = new DataHandler();
+        dataHandler.loadConfig();
+        this.permissionManager = new PermissionManager();
+        getServer().getPluginManager().registerEvents(new Events(), this);
+        getLogger().info("Loaded!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Bye!");
+    }
+
+    public static LumberJack getInstance() {
+        return main;
+    }
+}
